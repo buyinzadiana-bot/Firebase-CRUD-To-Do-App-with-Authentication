8810bcc8004b673aad69be58972e1e321a359908
DIANA <macbookpro@192.168.1.69>
2025-11-10 18:53:49 +0200

    first commit

---
+++ app/lib/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from 'firebase/auth'
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBzFSRezSR2elSZWUlvTWdvfggATi1jGek",
+  authDomain: "task-manager-68393.firebaseapp.com",
+  projectId: "task-manager-68393",
+  storageBucket: "task-manager-68393.firebasestorage.app",
+  messagingSenderId: "1006165407757",
+  appId: "1:1006165407757:web:87becb39194527e6ee3d44",
+  measurementId: "G-SS5M29TE8B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth= getAuth(app);
+export const db = getFirestore(app);
